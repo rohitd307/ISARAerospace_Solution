fa0780f6068f09e431868f1648b5ac141cfcb8ca
Rohit Dipankar <rohit.dipankar@sap.com>
2020-05-24 16:24:47 +0200

    Solution code

---
+++ ISARAerospace_Solution/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ISARAerospace_Solution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.toolDelete = new System.Windows.Forms.ToolTip(this.components);
+            this.toolLoad = new System.Windows.Forms.ToolTip(this.components);
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvBooks.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvBooks.BackgroundColor = System.Drawing.Color.FloralWhite;
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(39, 176);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.RowHeadersWidth = 57;
+            this.dgvBooks.Size = new System.Drawing.Size(712, 331);
+            this.dgvBooks.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnLoadData.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoadData.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadData.Location = new System.Drawing.Point(178, 103);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(156, 53);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "Load Data";
+            this.toolLoad.SetToolTip(this.btnLoadData, "Load Books data from csv file");
+            this.btnLoadData.UseVisualStyleBackColor = false;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(462, 103);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(156, 53);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete Books";
+            this.toolDelete.SetToolTip(this.btnDelete, "Deletes books which are Out of Stock");
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(178, 56);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(440, 22);
+            this.txtFilePath.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.134328F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Path:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Bauhaus 93", 8.059701F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(624, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 535);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.dgvBooks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ToolTip toolDelete;
+        private System.Windows.Forms.ToolTip toolLoad;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ISARAerospace_Solution/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using Microsoft.SqlServer.Server;
+
+namespace ISARAerospace_Solution
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnLoadData.Enabled = false;
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            bindData(txtFilePath.Text);
+        }
+
+        private void bindData(string filePath)
+        {
+            try
+            {
+                firstBind(filePath);
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Problem Occurred.!!\nApplication restarting...");
+                Application.Restart();
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                dgvBooks.DataSource = dt;
+                //dgvBooks.ReadOnly = true;
+                formatGridView();
+            }
+
+        }
+
+        private void firstBind(string filePath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            if (lines.Length > 0)
+            {
+                //first line to create header
+                string firstLine = lines[0];
+                string[] headerLabels = firstLine.Split(';');
+                foreach (string headerWord in headerLabels)
+                {
+                    dt.Columns.Add(new DataColumn(headerWord));
+                }
+                //For Data
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] dataWords = lines[i].Split(';');
+                    DataRow dr = dt.NewRow();
+                    int columnIndex = 0;
+                    DataGridViewComboBoxColumn binding = new DataGridViewComboBoxColumn();
+                    foreach (string headerWord in headerLabels)
+                    {
+                        dr[headerWord] = dataWords[columnIndex++];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+        }
+
+        private void formatGridView()
+        {
+            addComboBoxColumn();
+            addCheckBoxColumn();
+            addDescriptionButtonColumn();
+            formatPriceColumn();
+        }
+
+        private void addComboBoxColumn()
+        {
+            DataGridViewComboBoxCell bookBinding = new DataGridViewComboBoxCell();
+
+            var values = dt.AsEnumerable().Select(_ => _.Field<string>("Binding")).Distinct();
+            bookBinding.Items.AddRange(values.ToArray());
+
+            DataGridViewColumn cc = new DataGridViewColumn(bookBinding);
+            dgvBooks.Columns.Add(cc);
+            dgvBooks.Columns[7].HeaderText = dgvBooks.Columns[5].HeaderText;
+            dgvBooks.Columns[5].Visible = false;
+
+            foreach (DataGridViewRow item in dgvBooks.Rows)
+            {
+                item.Cells[7].Value = item.Cells[5].Value;
+                item.DefaultCellStyle.BackColor = Color.DarkGray;
+            }
+        }
+
+        private void addCheckBoxColumn()
+        {
+            string header = dgvBooks.Columns[4].HeaderText.ToString();
+            DataGridViewCheckBoxColumn dgvCmb = new DataGridViewCheckBoxColumn();
+
+            dgvCmb.ValueType = typeof(bool);
+            dgvCmb.Name = "Stock";
+            dgvCmb.HeaderText = header;
+
+            dgvBooks.Columns.Add(dgvCmb);
+            checkGridCombo();
+        }
+
+        private void checkGridCombo()
+        {
+            foreach (DataGridViewRow item in dgvBooks.Rows)
+            {
+                if (item.Cells[4].Value != null)
+                {
+                    if (item.Cells[4].Value.Equals("yes"))
+                        item.Cells[8].Value = true;
+                    else
+                    {
+                        item.Cells[8].Value = false;
+                        item.DefaultCellStyle.BackColor = Color.Yellow;
+                    }
+                }
+
+                dgvBooks.Columns[4].Visible = false;
+            }
+        }
+
+        private void addDescriptionButtonColumn()
+        {
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+
+            btn.HeaderText = "Description";
+            btn.Text = "Click Here";
+            btn.Name = "btnDescription";
+            btn.UseColumnTextForButtonValue = true;
+            dgvBooks.Columns.Add(btn);
+
+            dgvBooks.Columns[6].Visible = false;
+            dgvBooks.CellClick += new DataGridViewCellEventHandler(dgvBooks_CellClick);
+        }
+
+        private void dgvBooks_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                e.RowIndex >= 0)
+            {
+                foreach (DataGridViewRow item in dgvBooks.Rows)
+                {
+                    if (item.Cells[6].Value != null)
+                    {
+                        MessageBox.Show(item.Cells[6].Value.ToString());
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow item in dgvBooks.Rows)
+            {
+                if (item.Cells[8].Value != null)
+                {
+                    if (!Convert.ToBoolean(item.Cells[8].Value))
+                        dgvBooks.Rows.RemoveAt(item.Index);
+                }
+            }
+            dgvBooks.Refresh();
+        }
+
+        private void formatPriceColumn()
+        {
+            List<double> values = new List<double>();
+            foreach (DataGridViewRow item in dgvBooks.Rows)
+            {
+                if (item.Cells[3].Value != null)
+                    values.Add(double.Parse(item.Cells["Price"].Value.ToString()));
+            }
+            double[] price = values.ToArray();
+            Array.Sort(price);
+            Array.Reverse(price);
+
+            colorGradient(price);
+        }
+
+        private void colorGradient(double[] price)
+        {
+            Color textColor = Color.DarkRed;
+            foreach (double value in price)
+            {
+                foreach (DataGridViewRow item in dgvBooks.Rows)
+                {
+                    if (item.Cells[3].Value != null)
+                    {
+                        if (value == Convert.ToDouble(item.Cells[3].Value))
+                        {
+                            item.Cells[3].Style.ForeColor = textColor;
+                            textColor = ControlPaint.Light(textColor);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog1.FileName;
+                btnLoadData.Enabled = true;
+            }
+        }
+    }
+}
